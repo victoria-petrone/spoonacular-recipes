e50a6ac6058fac3970a922b7b7a03dc77addf4c7
Victoria Petrone <65722111+vico-design@users.noreply.github.com>
2020-12-19 19:55:52 +0100

    styled the button also  phone devices

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -17,27 +17,31 @@ const Home = () => {
           alt="image"
           src="https://www.judsonsmartliving.org/wp-content/uploads/Health-Foods-min.jpeg"
         />
-        <div className="inputs-container">
-          <input
-            placeholder="Hungry?"
-            className="input-element"
-            type="text"
-            onChange={(e) => setUserInput(e.currentTarget.value)}
-          />
-          {/* <input
+        <div className="inputs-link-container">
+          <div className="input-button-container">
+            <input
+              placeholder="Hungry?"
+              className="input-element"
+              type="text"
+              onChange={(e) => setUserInput(e.currentTarget.value)}
+            />
+            {/* <input
             placeholder="How many options?"
             className="input-element"
             type="number"
             onChange={(e) => setUserNumber(parseInt(e.currentTarget.value))}
           /> */}
-          <button
-            onClick={handleSubmit}
-            className="submit-button"
-            disabled={!userInput.length}
-          >
-            Search
-          </button>
-          <Link to="/search">Advanced Search</Link>
+            <button
+              onClick={handleSubmit}
+              className="submit-button"
+              disabled={!userInput.length}
+            >
+              Search
+            </button>
+          </div>
+          <div className="link-container">
+            <Link to="/search">Advanced Search</Link>
+          </div>
         </div>
       </div>
     </div>
