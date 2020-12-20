a1f83c2131052e530c1743b221c8e2f610a7cbea
Victoria Petrone <65722111+vico-design@users.noreply.github.com>
2020-12-20 17:21:49 +0100

    added social medias to the header, and making the logo responsive for mobiles

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -13,17 +13,38 @@ function Header() {
 
   return (
     <div className="header-container">
-      <img src={logo} alt="looogo" className="logo" />
+      <Link to="/">
+        <img src={logo} alt="looogo" className="logo" />
+      </Link>
+      <div className="links-container">
+        <Link to="/" className={getClassName("/")}>
+          Home
+        </Link>
+        <Link to="/favorite" className={getClassName("/favorite")}>
+          Favorites
+        </Link>
+        <Link to="/about" className={getClassName("/about")}>
+          About
+        </Link>
+      </div>
+      <div className="social-network-container">
+        <a href="https://github.com/vico-design" target="_blank">
+          <i className="ri-github-fill" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/maria-victoria-petrone/"
+          target="_blank"
+        >
+          <i className="ri-linkedin-box-fill" />
+        </a>
 
-      <Link to="/" className={getClassName("/")}>
-        Home
-      </Link>
-      <Link to="/favorite" className={getClassName("/favorite")}>
-        Favorites
-      </Link>
-      <Link to="/about" className={getClassName("/about")}>
-        About
-      </Link>
+        <a
+          href="https://www.xing.com/profile/MariaVictoria_Petrone/cv"
+          target="_blank"
+        >
+          <i className="ri-xing-fill" />
+        </a>
+      </div>
     </div>
   );
 }
