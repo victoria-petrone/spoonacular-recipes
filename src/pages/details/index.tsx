133df37939f07b7b3b188b6e36d411546b336cd8
Victoria Petrone <65722111+vico-design@users.noreply.github.com>
2020-12-20 22:54:28 +0100

    added ingredients list and style to the page

--- src/pages/details/index.tsx
+++ src/pages/details/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { getRecipeInformation } from "../../services/recipeSearch";
-
+import "./styles.css";
 interface IStep {
   length: { number: number; unit: string };
   number: number;
@@ -16,6 +16,7 @@ interface IRecipeDetails {
   vegetarian: boolean;
   glutenFree: boolean;
   analyzedInstructions: Array<{ name: string; steps: Array<IStep> }>;
+  extendedIngredients: Array<{ originalString: string; id: number }>;
 }
 
 const Details = () => {
@@ -39,9 +40,9 @@ const Details = () => {
     return recipeDetails?.analyzedInstructions?.[0].steps.map(
       ({ number, length, step }) => (
         <li key={number}>
-          <h2 className="step-number">{number}</h2>
+          <h3 className="step-number">{number}</h3>
           {length && (
-            <h4 className="step-duration">{length.number + length.unit}</h4>
+            <h5 className="step-duration">{length.number + length.unit}</h5>
           )}
           <p className="step-description">{step}</p>
         </li>
@@ -49,35 +50,51 @@ const Details = () => {
     );
   };
 
+  const getIngredients = () => {
+    return recipeDetails?.extendedIngredients.map(({ id, originalString }) => (
+      <li key={id}>
+        <p>{originalString}</p>
+      </li>
+    ));
+  };
+
   return (
-    <div>
+    <div className="recipe-detail-container">
       <h1>{recipeDetails?.title}</h1>
       <img src={recipeDetails?.image} />
-      <div>
-        <h3>Summary: </h3>
-        <div ref={summaryRef}></div>
+      <div className="all">
+        <div className="summary-container">
+          <h2>SUMMARY </h2>
+          <p ref={summaryRef}></p>
+        </div>
+        <div className="diet-information">
+          {recipeDetails?.vegan ? (
+            <i className="ri-check-line">Vegan</i>
+          ) : (
+            <i className="ri-close-fill">Vegan</i>
+          )}
+          <br />
+          {recipeDetails?.vegetarian ? (
+            <i className="ri-check-line">Vegetarian</i>
+          ) : (
+            <i className="ri-close-fill">Vegetarian</i>
+          )}
+          <br />
+          {recipeDetails?.glutenFree ? (
+            <i className="ri-check-line">GlutenFree</i>
+          ) : (
+            <i className="ri-close-fill">GlutenFree</i>
+          )}
+        </div>
+        <div className="ingredients-container">
+          <h2>INGREDIENTS</h2>
+          <ul>{getIngredients()}</ul>
+        </div>
+        <div className="instructions-container">
+          <h2>INSTRUCTIONS</h2>
+          <ul>{getInstructions()}</ul>
+        </div>
       </div>
-
-      <div className="diet-information">
-        {recipeDetails?.vegan ? (
-          <i className="ri-check-line">Vegan</i>
-        ) : (
-          <i className="ri-close-fill">Vegan</i>
-        )}
-        <br />
-        {recipeDetails?.vegetarian ? (
-          <i className="ri-check-line">Vegetarian</i>
-        ) : (
-          <i className="ri-close-fill">Vegetarian</i>
-        )}
-        <br />
-        {recipeDetails?.glutenFree ? (
-          <i className="ri-check-line">GlutenFree</i>
-        ) : (
-          <i className="ri-close-fill">GlutenFree</i>
-        )}
-      </div>
-      <ul className="instruction-container">{getInstructions()}</ul>
     </div>
   );
 };
