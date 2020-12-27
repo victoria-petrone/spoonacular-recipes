b6bd2ed32adb7f98c0bda8cb56a98715b95b9753
Victoria Petrone <65722111+vico-design@users.noreply.github.com>
2020-12-27 21:21:37 +0100

    added favorite icon, created select component and utils folder

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,47 +1,81 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { IComplexSearchConfig } from "../../services/recipeSearch";
+import Select from "../select";
 import "./styles.css";
+import filters from "../../utils/filters";
 
 interface ISearchProps {
   onSubmit?: (config: IComplexSearchConfig) => void;
   placeholder: string;
-  //optional
-  //value?: string;
+  initialValue: string | null;
 }
 
 const Search = (props: ISearchProps) => {
-  const [userInput, setUserInput] = useState("");
+  const [config, setConfig] = useState<IComplexSearchConfig>({
+    query: props.initialValue || "",
+  });
 
   const history = useHistory();
 
   const handleSubmit = () => {
     if (props.onSubmit) {
-      props.onSubmit({ query: userInput });
-      history.push(`/search?userInput=${userInput}`);
+      props.onSubmit(config);
     } else {
-      history.push(`/search?userInput=${userInput}`);
+      history.push(`/search?userInput=${config.query}`);
     }
   };
 
+  const sanitizeSelection = (selection: string[]) => {
+    let sanitizedSelection: string = "";
+    selection.forEach((item, i) => {
+      sanitizedSelection = sanitizedSelection + item;
+      if (selection.length - 1 !== i) {
+        sanitizedSelection = sanitizedSelection + ", ";
+      }
+    });
+
+    return sanitizedSelection;
+  };
+
+  const changeHandler = (filter: string, selection: string[]) => {
+    const newConfig = { ...config, [filter]: sanitizeSelection(selection) };
+    setConfig(newConfig);
+    props.onSubmit?.(newConfig);
+  };
+
   return (
-    <div className="input-button-container">
-      <input
-        onKeyDown={(e) => e.key === "Enter" && handleSubmit()}
-        placeholder={props.placeholder}
-        className="input-element"
-        type="text"
-        onChange={(e) => setUserInput(e.currentTarget.value)}
-        //value
-        //value={props.value}
-      />
-      <button
-        onClick={handleSubmit}
-        className="submit-button"
-        disabled={!userInput.length}
-      >
-        Search
-      </button>
+    <div className="search-container">
+      <div className="input-button-container">
+        <input
+          onKeyDown={(e) => e.key === "Enter" && handleSubmit()}
+          placeholder={props.placeholder}
+          className="input-element"
+          type="text"
+          onChange={(e) =>
+            setConfig({ ...config, query: e.currentTarget.value })
+          }
+          value={config.query}
+        />
+        <button
+          onClick={handleSubmit}
+          className="submit-button"
+          disabled={!config.query.length}
+        >
+          Search
+        </button>
+      </div>
+      <div className="filters-container">
+        {filters.map(({ name, title, options }) => (
+          <Select
+            key={name}
+            name={name}
+            title={title}
+            options={options}
+            onChange={changeHandler}
+          />
+        ))}
+      </div>
     </div>
   );
 };
