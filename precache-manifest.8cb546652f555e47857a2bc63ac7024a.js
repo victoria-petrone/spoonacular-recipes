26973d2636ac67a3c46612820e51e1437efb14de
Victoria Petrone <65722111+vico-design@users.noreply.github.com>
2021-01-18 11:58:00 +0100

    Updates

---
+++ precache-manifest.8cb546652f555e47857a2bc63ac7024a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3c6a8419e1eb64a6fdfaa15898c685",
+    "url": "/spoonacular-recipes/index.html"
+  },
+  {
+    "revision": "78ea1f80f89c1da8bda5",
+    "url": "/spoonacular-recipes/static/css/main.2c56db74.chunk.css"
+  },
+  {
+    "revision": "9f779fea01a4894b90e8",
+    "url": "/spoonacular-recipes/static/js/2.7db38244.chunk.js"
+  },
+  {
+    "revision": "eadc8ac8b6080df510389a008a76ce0a",
+    "url": "/spoonacular-recipes/static/js/2.7db38244.chunk.js.LICENSE"
+  },
+  {
+    "revision": "78ea1f80f89c1da8bda5",
+    "url": "/spoonacular-recipes/static/js/main.738a9da1.chunk.js"
+  },
+  {
+    "revision": "adafd33e3fe4bec1b030",
+    "url": "/spoonacular-recipes/static/js/runtime-main.a15d318a.js"
+  },
+  {
+    "revision": "cce4dd53fafbc20be8500baa00eb1941",
+    "url": "/spoonacular-recipes/static/media/pngwing.cce4dd53.png"
+  }
+]);
