a8ae84900ab0d4bc6fa95f7aaf4e3c8314d26425
Victoria Petrone <65722111+vico-design@users.noreply.github.com>
2021-02-20 19:59:04 +0100

    add basename to router for deploying

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import "./styles.css";
 import App from "./App";
 
 ReactDOM.render(
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
   </Router>,
   document.getElementById("root")
